47a4805e5a6c88d6baf8300fc166c08471e5575b
Nahom Bekele <nahom.fix@gmail.com>
2022-05-28 22:46:17 +0300

    wip: minor changes

--- apps/file_upload.py
+++ apps/file_upload.py
@@ -1,11 +1,18 @@
+import os
+
 import pandas as pd
 import streamlit as st
+from joblib import load
+
+pwd = os.getcwd()
+random_forest_model = os.path.join(pwd, "models/2022-05-28-17-13-16.pkl")
+lstm_model = os.path.join(pwd, "models/LSTM-2022-05-28-19-43-27.pkl")
 
 
 def app():
     with st.spinner("Loading Home ..."):
         data_file = st.file_uploader("Upload CSV", type=["csv"])
-        if st.button("Process"):
+        if st.button("Predict"):
             if data_file is not None:
                 file_details = {
                     "Filename": data_file.name,
@@ -16,3 +23,9 @@ def app():
 
                 df = pd.read_csv(data_file)
                 st.dataframe(df)
+
+                random_forest = load(random_forest_model)
+                lstm = load(lstm_model)
+
+                st.write(random_forest.predict(df))
+                # st.write(lstm.predict(df))
